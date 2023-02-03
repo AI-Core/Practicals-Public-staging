287d385e40bed750c1ce33ca83511b9d9e7dc631
IvanYingX <ying.yingxuan@hotmail.com>
2023-02-03 12:15:42 +0000

    Update public copy

--- Content/units/Towards ChatGPT/4. Pre-trained ML Models & Fine-Tuning/0. Pre-Trained Models/Practicals/0. Image Classification with ResNet50/Imports/utils_image.py
+++ Content/units/Towards ChatGPT/4. Pre-trained ML Models & Fine-Tuning/0. Pre-Trained Models/Practicals/0. Image Classification with ResNet50/Imports/utils_image.py
@@ -1,19 +1,19 @@
 from google.colab import files
 from PIL import Image
 
-def open_user_image():
+def open_user_image(transform):
     '''function to open the image, apply a transform, and return the image as a torch tensor'''
     print('Upload an image, or cancel to use the default image.')
     uploaded_file = files.upload()
     if uploaded_file:
         file_path = list(uploaded_file.keys())[0]
         file_extension = file_path.split('.')[-1]
-        assert file_extension in ['jpg', 'jpeg', 'png'], 'File must be a jpg, jpeg, or png.'
+        assert file_extension in ['jpg', 'jpeg', 'png'], '\nFile must be a jpg, jpeg, or png.'
         img = Image.open(file_path).convert('RGB')
-        print('Using the uploaded image.')
+        print('\nUsing the uploaded image.')
         display(img)
     else:
-        print('Using default image.')
+        print('\nUsing default image.')
         img = Image.open('duck.jpg').convert('RGB')
         display(img)
     img_tensor = transform(img).unsqueeze(0)
